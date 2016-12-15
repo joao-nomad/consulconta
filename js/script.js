1a132460111fa2fb8cc00823208e2e46749f094e
Joao Vitor <jvcadastro@gmail.com>
2016-12-15 17:20:58 -0200

    Primeiro Commit
    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,151 @@
+$("#laudoPericial").click(function() {
+  window.location.href = 'services/laudoPericial.html';
+});
+
+$("#laudoPericial").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#laudoPericial").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#EC7514');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#parecerPericial").click(function() {
+  window.location.href = 'services/parecerPericial.html';
+});
+
+$("#parecerPericial").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#parecerPericial").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#EC7514');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#apHaveres").click(function() {
+  window.location.href = 'services/apuracaoHaveres.html';
+});
+
+$("#apHaveres").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#apHaveres").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#EC7514');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#cartaoCredito").click(function() {
+  window.location.href = 'services/cartaoCredito.html';
+});
+
+$("#cartaoCredito").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#cartaoCredito").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#EC7514');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#dissolucaoSociedades").click(function() {
+  window.location.href = 'services/dissolucaoSociedades.html';
+});
+
+$("#dissolucaoSociedades").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#dissolucaoSociedades").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#EC7514');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#fundoComercio").click(function() {
+  window.location.href = 'services/fundoComercio.html';
+});
+
+$("#fundoComercio").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#fundoComercio").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#EC7514');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#exclusaoSocios").click(function() {
+  window.location.href = 'services/exclusaoSocios.html';
+});
+
+$("#exclusaoSocios").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#exclusaoSocios").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#EC7514');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#financeiroHabitacao").click(function() {
+  window.location.href = 'services/financeiroHabitacao.html';
+});
+
+$("#financeiroHabitacao").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#financeiroHabitacao").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#EC7514');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#sucessaoHereditaria").click(function() {
+  window.location.href = 'services/sucessaoHereditaria.html';
+});
+
+$("#sucessaoHereditaria").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#sucessaoHereditaria").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#EC7514');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#prestacaoContas").click(function() {
+  window.location.href = 'services/prestacaoContas.html';
+});
+
+$("#prestacaoContas").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#prestacaoContas").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#EC7514');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#contratosFinanceiros").click(function() {
+  window.location.href = 'services/contratosFinanceiros.html';
+});
+
+$("#contratosFinanceiros").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#contratosFinanceiros").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#EC7514');   
+});
