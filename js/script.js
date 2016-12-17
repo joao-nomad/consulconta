99717acbfa8549e437ea5909e65e42eac824c85f
Joao Vitor <jvcadastro@gmail.com>
2016-12-17 09:50:12 -0200

    Primeira coluna

--- js/script.js
+++ js/script.js
@@ -11,3 +11,59 @@ $("#orientacaoGestao").mouseleave(function() {
 });
 
 //--------------------------------------------------------------------------------------------//
+
+$("#diagnostico").click(function() {
+  window.location.href = 'services/diagnostico.html';
+});
+
+$("#diagnostico").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#diagnostico").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#367100');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#recuperacao").click(function() {
+  window.location.href = 'services/recuperacao.html';
+});
+
+$("#recuperacao").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#recuperacao").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#367100');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#renegociacao").click(function() {
+  window.location.href = 'services/renegociacao.html';
+});
+
+$("#renegociacao").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#renegociacao").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#367100');   
+});
+
+//--------------------------------------------------------------------------------------------//
+
+$("#avaliacaoBens").click(function() {
+  window.location.href = 'services/avaliacaoBens.html';
+});
+
+$("#avaliacaoBens").hover(function() {
+  $(this).children().children('span').children().css('background-color', 'black');
+});
+
+$("#avaliacaoBens").mouseleave(function() {
+  $(this).children().children('span').children().css('background-color', '#367100');   
+});
+
+//--------------------------------------------------------------------------------------------//
